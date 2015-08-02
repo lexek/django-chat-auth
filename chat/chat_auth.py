4d88bd6cfb29a77886f986cbcfb8e33deeefcc6c
Белостоцкий Алексей <lexek-92@ya.ru>
2015-08-02 19:49:17 +0300

    постфикс для имён + кавычки

--- chat/chat_auth.py
+++ chat/chat_auth.py
@@ -16,13 +16,13 @@ class ChatBackend(object):
         if r.status_code == 200:
             data = r.json()
             try:
-                chat_user = ChatUser.objects.get(id=data["user"]["id"])
+                chat_user = ChatUser.objects.get(id=data['user']['id'])
                 self.__fill_user(chat_user.user, data, password)
             except ChatUser.DoesNotExist:
                 user = User.objects.create_user(username)
                 self.__fill_user(user, data, password)
                 chat_user = ChatUser()
-                chat_user.id = data["user"]["id"]
+                chat_user.id = data['user']['id']
                 chat_user.user = user
                 chat_user.save()
             return chat_user.user
@@ -31,10 +31,10 @@ class ChatBackend(object):
 
     @staticmethod
     def __fill_user(user, data, password):
-        role = data["user"]["role"]
+        role = data['user']['role']
         user.password = make_password(password)
-        user.username = data["user"]["name"]
-        admin = role == "SUPERADMIN"
+        user.username = data['user']['name'] + '@chat'
+        admin = role == 'SUPERADMIN'
         user.is_superuser = user.is_superuser or admin
         user.is_staff = user.is_staff or admin
         user.is_active = True
